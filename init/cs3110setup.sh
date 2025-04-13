518e684f69ab15be6eeb9c382e563323e9014f9e
aximaris <73300175+kwfcfc@users.noreply.github.com>
2025-04-13 04:50:45 +0000

    feat: ocaml setup for Cornel cs3110
    1. a simple devcontainer.json to set up ocaml and
       vscodium
    2. a simple init bash script to install packages
    3. a init.ml to test vscodium lsp functions

---
+++ init/cs3110setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eux  # exit on error
+
+opam switch create cs3110-2025sp ocaml-base-compiler.5.2.0
+eval $(opam env)
+opam install -y utop odoc ounit2 qcheck bisect_ppx menhir ocaml-lsp-server ocamlformat
+
