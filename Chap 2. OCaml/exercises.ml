54966525e494ede5eb87eb1ba324268fb0c9e149
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-04-15 20:45:54 +0000

    feat: add exercises for Chapter 2

---
+++ Chap 2. OCaml/exercises.ml
@@ -0,0 +1,55 @@
+(** Exercise: values *)
+let _:int = 7 * (1 + 2 + 3)
+(* - : int = 42 *)
+
+let _:string = "CS " ^ string_of_int 3110
+(* - : string = "CS 3119" *)
+
+(** Exercise: operators *)
+let _ = 42 * 10
+let _ = 3.14 /. 2.
+let _ = 4.2 ** 7.
+
+(** Exercise: equality *)
+let _ = 42 = 42
+let _ = "hi" = "hi"
+let _ = "hi" == "hi"
+
+(** Exercise: assert *)
+(* assert true;;
+   - : unit = () *)
+(* assert false;;
+  Exception: Assert_failure ... *)
+let _ = assert (2110 <> 3110)
+
+(** Exercise: if *)
+let _ = if 2 > 1 then 42 else 7
+
+(** Exercise: double fun *)
+let double x = x * 2
+
+let _ = assert (double 7 = 14)
+let _ = assert (double 0 = 0)
+let _ = assert (double (-2) = -4)
+
+(** Exercise: more fun *)
+let cube x = x *. x *. x
+
+let _ = assert (cube 1.0 = 1.0)
+let _ = assert (cube 2.5 = 15.625)
+
+let sign x = if x > 0 then 1 
+  else if x == 0 then 0 else -1
+
+let _ = assert (sign 3 = 1)
+let _ = assert (sign 0 = 0)
+let _ = assert (sign (-6) = -1)
+
+let area r = Float.pi *. r *. r
+
+let close_enough ?(delta = 1e-5) a b = 
+  Float.abs (a -. b) < delta
+
+let _ = assert (close_enough (area 1.) Float.pi)
+let _ = assert (close_enough (area 2.) (Float.pi *. 4.))
+let _ = assert (close_enough (area 1.5) (Float.pi *. 2.25))
