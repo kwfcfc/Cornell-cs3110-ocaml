2dd13520b9bd3794a4de1e6a88ef41f763d06811
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-04-21 04:31:07 +0000

    refactor: clean up cache after opam install

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,3 +5,5 @@ set -eux  # exit on error
 opam switch create cs3110-2025sp ocaml-base-compiler.5.2.0
 eval $(opam env)
 opam install -y utop odoc ounit2 qcheck bisect_ppx menhir ocaml-lsp-server ocamlformat
+# clean up caches and log files
+opam clean
