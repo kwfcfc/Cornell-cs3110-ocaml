30d604f939aa7de951aa56b1a2d57c99490d822c
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-04-22 04:44:57 +0000

    refactor: add a cleanup command after opam install

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,5 +5,5 @@ set -eux  # exit on error
 opam switch create cs3110-2025sp ocaml-base-compiler.5.2.0
 eval $(opam env)
 opam install -y utop odoc ounit2 qcheck bisect_ppx menhir ocaml-lsp-server ocamlformat
-# clean up caches and log files
-opam clean
+# clean up caches and log files, -a for all switches and c for downloaded caches
+opam clean -a -c
